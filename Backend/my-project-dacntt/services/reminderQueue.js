0b3c813c1802150d62c2b11598287c1b0f47e0ee
VoVanThuan2004 <vot04766@gmail.com>
2025-12-08 23:03:46 +0700

    Fix url redis

--- Backend/my-project-dacntt/services/reminderQueue.js
+++ Backend/my-project-dacntt/services/reminderQueue.js
@@ -8,12 +8,18 @@ require("dotenv").config();
 const { sendTaskDeadlineEmail } = require("../config/mailConfig");
 
 // 🔌 Kết nối Redis
-const connection = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  maxRetriesPerRequest: null, // ⚠️ Bắt buộc cho BullMQ
+// const connection = new Redis({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT,
+//   maxRetriesPerRequest: null,
+//   enableReadyCheck: false,
+// });
+
+const connection = new Redis(process.env.REDIS_URL || "", {
+  maxRetriesPerRequest: null,
   enableReadyCheck: false,
 });
+
 
 // Queue để thêm job
 const reminderQueue = new Queue("taskReminderQueue", { connection });
